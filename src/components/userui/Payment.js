254883037e9c4e314a4c8706d624ef9ccaaf7b7d
Sanjeev_works <sanjeevmanagutti@gmail.com>
2022-07-12 13:00:16 +0530

    making simple changes

--- src/components/userui/Payment.js
+++ src/components/userui/Payment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from "axios";
 import {useContext,useState } from "react";
 import { useHistory} from "react-router-dom";
 import {context}  from '../../App'
@@ -8,6 +9,51 @@ import './payment.css'
 import { v4 as uuid } from 'uuid';
 
 export  function Payment() {
+
+  const [book, setBook] = useState({
+		name: "The Fault In Our Stars",
+		author: "John Green",
+		img: "https://images-na.ssl-images-amazon.com/images/I/817tHNcyAgL.jpg",
+		price: 250,
+	});
+	
+	const initPayment = (data) => {
+		const options = {
+			key: "rzp_test_GdM9bftAUebbNz",
+			amount: data.amount,
+			currency: data.currency,
+			name: book.name,
+			description: "Test Transaction",
+			image: book.img,
+			order_id: data.id,
+			handler: async (response) => {
+				try {
+					const verifyUrl = "http://localhost:8080/api/payment/verify";
+					const { data } = await axios.post(verifyUrl, response);
+					console.log(data);
+				} catch (error) {
+					console.log(error);
+				}
+			},
+			theme: {
+				color: "#3399cc",
+			},
+		};
+		//const rzp1 = new window.Razorpay(options);
+		//rzp1.open();
+	};
+
+	const handlePayment = async () => {
+		try {
+			const orderUrl = "http://localhost:8080/api/payment/orders";
+			const { data } = await axios.post(orderUrl, { amount: book.price });
+			console.log(data);
+			initPayment(data.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 const {uname,mName,time,tName,seats} = useContext(context);
 const [show, setShow] = useState(false);
   const summaryStyle = { display: show ? "block" : "none" }
@@ -55,7 +101,10 @@ const dseat=seats.toString()
         <h5>@{tName}</h5>
     </div>
 </div>
-    <Button color="success" variant="outlined" onClick={addbooking}>PAY</Button>
+<div>
+ <img draggable="false" className="h-6 w-6 object-contain" src="https://rukminim1.flixcart.com/www/96/96/promos/01/09/2020/a07396d4-0543-4b19-8406-b9fcbf5fd735.png" alt="Paytm Logo" />
+ </div>
+    <Button color="success" variant="outlined" onClick={handlePayment}>PAY</Button>
       {show ? <Alert style={summaryStyle} className="Aletr" severity="success">Booking is Successful!</Alert> : ""} 
 </div>
     );
